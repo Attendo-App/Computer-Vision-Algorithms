5264348225436c11a8a2a2f6e08e2f9a9da034e0
Angad Kambli <angad-k@users.noreply.github.com>
2020-12-03 14:49:26 +0530

    implement naive sobel

---
+++ sobeledgedetectionnaive.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def sobel(img):
+    
+
+    #gray_img = np.round(0.299 * img[:, :, 0] + 0.587 * img[:, :, 1] + 0.114 * img[:, :, 2]).astype(np.uint8)
+
+    h, w = img.shape
+    horizontal = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    vertical = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+    gradient_image = np.zeros((480, 640))
+
+    for i in range (1, h - 1):
+        for j in range (1, w - 1):
+            horizontalDiff = 0
+            verticalDiff = 0
+            for k in range (-1, 1):
+                for l in range (-1, 1):
+                    horizontalDiff += horizontal[1 + k][1 + l] * img[i + k][j + l]
+                    verticalDiff += vertical[1 + k][1 + l] * img[i + k][j + l]
+            gradient_image[i-1][j-1] = np.sqrt(pow(horizontalDiff, 2.0) + pow(verticalDiff, 2.0))
+    return gradient_image
+
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import numpy as np
 import cv2 as cv
+import sobeledgedetectionnaive as sobelnaive
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+x = 0 
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -13,8 +15,15 @@ while True:
         break
     # Our operations on the frame come here
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    edges = sobelnaive.sobel(gray)
+    if(x == 0):
+        print(type(gray))
+        print(edges)
+        x = x + 1
+    
     # Display the resulting frame
-    cv.imshow('frame', gray)
+    #edges = cv.cvtColor(edges, cv.COLOR_GRAY2BGR)
+    cv.imshow('frame', edges.astype('uint8'))
     if cv.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
