5a45d384e1b3429107605b210a0fcc9a17621d4d
Angad Kambli <angad-k@users.noreply.github.com>
2020-12-03 19:44:29 +0530

    code cleanup

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import numpy as np
+import pycuda
 import cv2 as cv
-import sobeledgedetectionnaive as sobelnaive
+import Sobel_Edge_Detect_Naive as sobelNaive
+
 cap = cv.VideoCapture(0)
+
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -9,23 +12,23 @@ x = 0
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
+
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
+
     # Our operations on the frame come here
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    edges = sobelnaive.sobel(gray)
-    if(x == 0):
-        print(type(gray))
-        print(edges)
-        x = x + 1
-    
+    edges = sobelNaive.sobel(gray)
+
     # Display the resulting frame
-    #edges = cv.cvtColor(edges, cv.COLOR_GRAY2BGR)
     cv.imshow('frame', edges.astype('uint8'))
+
+    #exit on press q
     if cv.waitKey(1) == ord('q'):
         break
+
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
